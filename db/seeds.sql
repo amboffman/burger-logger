f926dc63a7f727b0097cea8eca06379d8d5b148c
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-17 17:55:12 -0700

    Created a database schema and a connection module

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Let It Snow Peas Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tunami');
+INSERT INTO burgers (burger_name) VALUES ('Beets Me Burger');
