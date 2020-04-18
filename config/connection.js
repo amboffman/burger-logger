cb9ed764f59595855ce0cdad792e467c55000744
Anthony Boffman <amarrokalboffman@gmail.com>
2020-04-17 19:49:27 -0700

    Update connection for Heroku Deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL) {conection = mysql.createConnection(process.env.JAWSDB_URL)}
+
+else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
 });
+}
 
 connection.connect((err) => {
   if (err) {
